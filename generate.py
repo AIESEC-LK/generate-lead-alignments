72a4cdbe899026cb03ba25f4ccef6f69e025b848
Fouzul Hassan <72754118+fouzul-hassan@users.noreply.github.com>
2024-07-04 22:18:57 +0530

    Create generate.py

---
+++ generate.py
@@ -0,0 +1,28 @@
+from urllib.request import urlopen
+import json
+
+EY_ID = ENTITY CODE HERE // add your entity code here Eg: EY_ID = 1623
+url = "https://gis-api.aiesec.org/v2/lists/mcs_alignments.json?mc_id[]=495&amp;mc_id[]=537"
+page = urlopen(url)
+json_str = page.read()
+json_obj = json.loads(json_str)
+
+
+
+for entity in json_obj:
+    if (entity['id'] != EY_ID):
+        continue
+
+    f = open("alignmentsVIETNAM.txt", "w", encoding='utf-8')
+
+    alignments = entity['alignments']
+    for alignment in alignments:
+        data_id = alignment['alignment_id']
+        name = alignment['value']
+        value = alignment['id']
+        line = f'<option data-id="{data_id}" value="{value}">{name}</option>\n'
+        print(line)
+        f.write(line)
+
+    f.close()
+
